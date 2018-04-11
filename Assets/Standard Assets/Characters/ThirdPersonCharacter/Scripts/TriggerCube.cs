1dfa8f2ad36b8686b59e54d9086f2793f2b2af67
patrykpp <patryk583@vp.pl>
2018-04-11 21:41:03 +0200

    lab4_GiM
    Grafika i Multimedia

---
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/TriggerCube.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using UnityEngine.UI;
+
+
+public class TriggerCube : MonoBehaviour {
+
+
+    public Text countText;
+    public Text winText;
+    public Light lt ;
+    public int licznik = 0;
+
+    public void Start()
+    {
+        //lt = GetComponent<Light>();
+        winText.text = "";
+        countText.text = "";
+
+
+    }
+
+    public void Update()
+    {
+        countText.text = "Punkty: " + licznik.ToString();
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+
+
+        if (other.gameObject.CompareTag("Cube"))
+        {
+			lt.color = Color.white / 2.0F * Time.deltaTime;
+			Destroy(other.gameObject);
+			licznik++;
+			Update();
+
+			if (licznik > 4)
+			{
+				winText.text = "Wygrałeś !";
+				Time.timeScale = 0;
+			}
+
+
+        }
+
+    }
+}
+
